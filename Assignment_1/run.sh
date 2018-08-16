abae6d3d6b3194bd985db296cd807b1a237bdbc7
Max Bo <max@maxbo.me>
2018-08-16 21:29:28 +1000

    Fix parsing regime

--- Assignment_1/run.sh
+++ Assignment_1/run.sh
@@ -1,6 +1,6 @@
-hash=`git rev-parse HEAD`
-name="$hash"
+hash=`git rev-parse --short HEAD`
+name="$1-$hash"
 
-make && ./nbody 6000000 0.016666666666666666 10 $1 > "outputs/$name.data" 
-# python3 viz.py "outputs/$name.data" "outputs/$name.mp4" && open "outputs/$name.mp4"
+make && ./nbody 6000000 0.016666666666666666 10 "inputs/$1" > "outputs/$name.data" 
+python3 viz.py "outputs/$name.data" "outputs/$name.mp4" && open "outputs/$name.mp4"
 
