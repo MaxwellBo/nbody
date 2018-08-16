666701a8fc60d19340d23b31fdb2e942c59b80ec
Max Bo <max@maxbo.me>
2018-08-17 01:59:22 +1000

    Add export notifier

--- Assignment_1/run.sh
+++ Assignment_1/run.sh
@@ -2,5 +2,6 @@ hash=`git rev-parse --short HEAD`
 name="$1-$hash"
 
 make && ./nbody 6000000 0.016666666666666666 10 "inputs/$1" > "outputs/$name.data" 
+echo "Exported data to ./outputs/$name.data"
 python3 viz.py "outputs/$name.data" "outputs/$name.mp4" && open "outputs/$name.mp4"
 
